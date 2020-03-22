5e0cae88696aaeb90a777ca9c294c36d465de3eb
rtavaresramos <rtavareskxd99@gmail.com>
2020-03-21 23:56:57 -0300

    Adjusting multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,5 +3,33 @@ const path = require('path')
 const crypto = require('crypto')
 
 module.exports = {
+    dest: path.resolve(__dirname, '..', '..' , 'tmp' , 'uploads'),
+    storage: multer.diskStorage({
+        destination: (req, file, cb)=>{
+            cb(null, path.resolve(__dirname, '..', '..' , 'tmp' , 'uploads') )
+        },
+        filename: (req, file, cb)=>{
+            crypto.randomBytes(16, (err, hash) =>{
 
+            })
+        }
+    }),
+     limits:{
+        fileSize: 2 * 1024 * 1024,
+     },
+
+     fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif',
+        ]
+
+        if(allowedMimes.includes(file.mimetype)){
+            cb(null,  true)
+        }else{
+            cb( new Error('Invalid file type'))
+        }
+     },
 }
