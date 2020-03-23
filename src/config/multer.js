d69c4d66a919a0915ee969974e195e0f35ca80d1
rtavaresramos <rtavareskxd99@gmail.com>
2020-03-23 15:39:50 -0300

    Conection with amazon s3 is already working

--- src/config/multer.js
+++ src/config/multer.js
@@ -23,7 +23,7 @@ const storageTypes = {
 
     s3: multerS3({
         s3: new aws.S3(), 
-        bucket: 'rtavaresramos',
+        bucket: 'uploader-rtr',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         acl: 'public-read',
         key: (req, file, cb) =>{
@@ -40,6 +40,7 @@ const storageTypes = {
 
 module.exports = {
     dest: path.resolve(__dirname, '..', '..' , 'tmp' , 'uploads'),
+    storage: storageTypes["s3"],
      limits:{
         fileSize: 2 * 1024 * 1024,
      },
--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,32 @@ const multerConfig = require('./config/multer')
 
 
 const Post = require('./models/Post')
+routes.get('/posts', async(req, res) =>{
+    const post = await Post.find()
+
+    return res.json(post)
+})
+
 
 routes.post('/posts', multer(multerConfig).single('file'), async(req , resp) => {
-    const {originalname: name, size, key} =req.file
+    const {originalname: name, size, key, location: url = ""} =req.file
 
     const post = await Post.create({
         name,
         size,
         key,
-        url:'',
+        url,
     })
 
     return resp.json(post)
 })
 
+routes.delete('/posts/:id', async(req, res) => {
+    const post = await Post.findById(req.params.id)
+
+    await post.remove()
+
+    return res.send()
+})
 
 module.exports = routes
